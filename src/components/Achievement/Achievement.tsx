87cc7b05665e20bf878eb1dcb481c3d0ef9045fb
Victoria <bika0724@gmail.com>
2022-06-28 19:10:33 +0300

    Сделала компонент ачивок

---
+++ src/components/Achievement/Achievement.tsx
@@ -0,0 +1,25 @@
+import Icon from "@components/Icon/Icon";
+import styles from "./Achievement.module.scss";
+
+interface AchievementProps {
+    level?: number;
+    image: string;
+    alt?: string;
+}
+
+const Achievement = ({ level, image, alt = "" }: AchievementProps) => {
+    return (
+        <div className={styles.achievement}>
+            {level && <div className={styles.achievement__level}>{level}</div>}
+            <div className={styles.achievement__image}>
+                <img
+                    className={styles.achievement__image__icon}
+                    src={image}
+                    alt={alt}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Achievement;
