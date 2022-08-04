4a4ee1730512e5e4641e764bdb04cf1a38bce5f8
Victoria <bika0724@gmail.com>
2022-08-04 12:35:40 +0300

    Страница матчи

---
+++ src/pages/matches/index.tsx
@@ -0,0 +1,7 @@
+import Layout from "@layouts/Layout/Layout";
+
+const Matches = () => {
+    return <Layout>Матчи</Layout>;
+};
+
+export default Matches;
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,7 +1,4 @@
 import Layout from "@layouts/Layout/Layout";
-import { Widget } from "@features/Widget/Widget";
-import { WidgetLastMatches } from "@features/Widget/WidgetLastMatches/WidgetLastMatches";
-import Achievement from "@components/Achievement/Achievement";
 import ProfileLayout from "@layouts/Profile/ProfileLayout";
 
 const Profile = () => {
